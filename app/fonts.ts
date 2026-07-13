c60140eeaf263942b5cea81a3ad99b5c4b61af46
xicaaay <xic444y@gmail.com>
2026-07-13 01:12:16 -0600

    feat: configurar tipografías locales del portafolio

---
+++ app/fonts.ts
@@ -0,0 +1,25 @@
+import localFont from "next/font/local";
+
+export const archivoBlack = localFont({
+  src: "./fonts/ArchivoBlack-Regular.ttf",
+  variable: "--font-archivo-black",
+  weight: "400",
+  style: "normal",
+  display: "swap",
+});
+
+export const geist = localFont({
+  src: "./fonts/Geist-Regular.ttf",
+  variable: "--font-geist",
+  weight: "400",
+  style: "normal",
+  display: "swap",
+});
+
+export const jetBrainsMono = localFont({
+  src: "./fonts/JetBrainsMono-SemiBold.ttf",
+  variable: "--font-jetbrains-mono",
+  weight: "600",
+  style: "normal",
+  display: "swap",
+});
